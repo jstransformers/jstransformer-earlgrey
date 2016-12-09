62cdaa06683b58954df734ad6c2fc6488f27b690
Rob Loach <robloach@gmail.com>
2016-12-09 13:27:45 -0500

    Add xo as coding style (#37)

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ exports.outputFormat = 'js'
 exports.render = function (str, options) {
   options = options || {}
   options.es5 = true
+  // eslint-disable-next-line new-cap
   var gen = eg.Generator(options)
+  // eslint-disable-next-line new-cap
   var code = gen.generate(eg.Source(str))
   return code.code
 }
